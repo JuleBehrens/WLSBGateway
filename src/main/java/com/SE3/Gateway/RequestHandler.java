66923a4c62d829bc5df9f09cfdf4698b5dc75630
Jonah Schierding <jonah.schierding@live.de>
2021-06-14 22:24:49 +0200

    Added tomorrow.io weather API

--- src/main/java/com/SE3/Gateway/RequestHandler.java
+++ src/main/java/com/SE3/Gateway/RequestHandler.java
@@ -40,4 +40,23 @@ public class RequestHandler {
 		String url = String.format("http://localhost:8081/api/schedule?nap=%s&age=%s&breakfast=%s&wakeUpTime=%s&getReadyDuration=%s&workingHours=%s",""+nap,""+age,""+breakfast,wakeUpTime, getReadyDuration, workingHours);
 		return HTTPClient.getRequest(url);
 	}
+
+	/**
+	 * Handles Get-Requests for weather data, for more information also see https://docs.tomorrow.io/reference/api-introduction and https://docs.tomorrow.io/reference/data-layers-core
+	 * @param location Coordinates of the location in format 'latitude,longitude' (Example: 49.01079,8.40865)
+	 * @param startTime First Timestamp for which data is retrieved in  ISO 8601 format (Example '2019-03-20T14:09:50Z')
+	 * @param endTime Last Timestamp for which data is retrieved in  ISO 8601 format (Example '2019-03-20T14:09:50Z')
+	 * @param timeSteps Intervals for which data is retrieved, for format see https://docs.tomorrow.io/reference/data-layers-overview
+	 * @param timeZone TimeZone in which timestamps are returned, for format see https://docs.tomorrow.io/reference/api-formats
+	 * @return Answer in JSON with weather data (Temperature, Apperent Temperatue, Weather Code, Precipitation Intensity, Precipitation Type, Wind Speed) from https://www.tomorrow.io/
+	 */
+	@GetMapping("/api/weather")
+	public String weather(	@RequestParam(value = "location") String location,
+							@RequestParam(value = "startTime") String startTime,
+							@RequestParam(value = "endTime") String endTime,
+							@RequestParam(value = "timesteps") String timeSteps,
+							@RequestParam(value = "timezone") String timeZone) {
+		String url = String.format("https://api.tomorrow.io/v4/timelines?apikey=dIdeyiuMKXGdmuPO5nOfrwmh3d1BFLbo&location=%s&fields=temperature,temperatureApparent,weatherCode,precipitationIntensity,precipitationType,windSpeed&startTime=%s&endTime=%s&timesteps=%s&timezone=%s", location, startTime, endTime, timeSteps, timeZone);
+		return HTTPClient.getRequest(url);
+	}
 }
